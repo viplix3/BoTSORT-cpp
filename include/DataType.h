b6b04b7e247273604f929d6a81995ce67dc20fe7
dumb af <vipin.sharma2024@gmail.com>
2023-05-05 17:16:37 +0400

    changed dtype names

--- include/DataType.h
+++ include/DataType.h
@@ -5,23 +5,14 @@
 #include <eigen3/Eigen/Core>
 #include <eigen3/Eigen/Dense>
 
-constexpr uint8_t DET_ELEMENTS = 6;
+constexpr uint8_t DET_ELEMENTS = 4;
 constexpr uint8_t FEATURE_DIM = 128;
 constexpr uint8_t KALMAN_STATE_SPACE_DIM = 8;
 constexpr uint8_t KALMAN_MEASUREMENT_SPACE_DIM = 4;
 
 
-// Object Detection
-/**
- * @brief Detection [x1, y1, x2, y2, class_id, score]
- * 
- */
-typedef Eigen::Matrix<float, 1, DET_ELEMENTS, Eigen::RowMajor> DET;
-/**
- * @brief Detection matrix [num_detections, DET_ELEMENTS]
- * 
- */
-typedef Eigen::Matrix<float, Eigen::Dynamic, DET_ELEMENTS, Eigen::RowMajor> DET_MATRIX;
+typedef Eigen::Matrix<float, 1, DET_ELEMENTS> DET_VEC;
+typedef Eigen::Matrix<float, Eigen::Dynamic, DET_ELEMENTS> DET_MATRIX;
 
 
 // Re-ID features
@@ -29,48 +20,46 @@ typedef Eigen::Matrix<float, Eigen::Dynamic, DET_ELEMENTS, Eigen::RowMajor> DET_
  * @brief Re-ID feature vector
  * 
  */
-typedef Eigen::Matrix<float, 1, FEATURE_DIM, Eigen::RowMajor> FEATURE_VECTOR;
+typedef Eigen::Matrix<float, 1, FEATURE_DIM> FEATURE_VECTOR;
 /**
  * @brief Re-ID feature vector matrix [num_features, FEATURE_DIM]
  * 
  */
-typedef Eigen::Matrix<float, Eigen::Dynamic, FEATURE_DIM, Eigen::RowMajor> FEATURE_MATRIX;
+typedef Eigen::Matrix<float, Eigen::Dynamic, FEATURE_DIM> FEATURE_MATRIX;
 
 
 // Kalman Filter
 /**
- * @brief Kalman filter mean vector (state space)
- *  BoT-SORT uses 8 dimensional state space [x, y, w, h, vx, vy, vw, vh]
+ * @brief 1xKALMAX_STATE_SPACE_DIM vector
  * 
  */
-typedef Eigen::Matrix<float, 1, KALMAN_STATE_SPACE_DIM, Eigen::RowMajor> KF_MEAN_STATE_SPACE;
+typedef Eigen::Matrix<float, 1, KALMAN_STATE_SPACE_DIM> KF_STATE_SPACE_VEC;
 /**
- * @brief Kalman filter covariance matrix (state space)
+ * @brief KALMAN_STATE_SPACE_DIMxKALMAN_STATE_SPACE_DIM matrix
  * 
  */
-typedef Eigen::Matrix<float, KALMAN_STATE_SPACE_DIM, KALMAN_STATE_SPACE_DIM, Eigen::RowMajor> KF_COVARIANCE_STATE_SPACE;
+typedef Eigen::Matrix<float, KALMAN_STATE_SPACE_DIM, KALMAN_STATE_SPACE_DIM> KF_STATE_SPACE_MATRIX;
 /**
  * @brief Kalman filter state space data [mean, covariance]
  * 
  */
-using KF_DATA_STATE_SPACE = std::pair<KF_MEAN_STATE_SPACE, KF_COVARIANCE_STATE_SPACE>;
+using KF_DATA_STATE_SPACE = std::pair<KF_STATE_SPACE_VEC, KF_STATE_SPACE_MATRIX>;
 
 /**
- * @brief Kalman filter mean vector (measurement space).
- *  In BoT-SORT, measurement space is 4 dimensional [x, y, w, h]
+ * @brief 1xKALMAN_MEASUREMENT_SPACE_DIM vector
  * 
  */
-typedef Eigen::Matrix<float, 1, KALMAN_MEASUREMENT_SPACE_DIM, Eigen::RowMajor> KF_MEAN_MEASUREMENT_SPACE;
+typedef Eigen::Matrix<float, 1, KALMAN_MEASUREMENT_SPACE_DIM, Eigen::RowMajor> KF_MEAS_SPACE_VEC;
 /**
- * @brief Kalman filter covariance matrix (measurement space)
+ * @brief KALMAN_MEASUREMENT_SPACE_DIMxKALMAN_MEASUREMENT_SPACE_DIM matrix
  * 
  */
-typedef Eigen::Matrix<float, KALMAN_MEASUREMENT_SPACE_DIM, KALMAN_MEASUREMENT_SPACE_DIM, Eigen::RowMajor> KF_COVARIANCE_MEASUREMENT_SPACE;
+typedef Eigen::Matrix<float, KALMAN_MEASUREMENT_SPACE_DIM, KALMAN_MEASUREMENT_SPACE_DIM, Eigen::RowMajor> KF_MEAS_SPACE_MATRIX;
 /**
  * @brief Kalman filter measurement space data [mean, covariance]
  * 
  */
-using KF_DATA_MEASUREMENT_SPACE = std::pair<KF_MEAN_MEASUREMENT_SPACE, KF_COVARIANCE_MEASUREMENT_SPACE>;
+using KF_DATA_MEASUREMENT_SPACE = std::pair<KF_MEAS_SPACE_VEC, KF_MEAS_SPACE_MATRIX>;
 
 
 // Tracker
@@ -108,4 +97,4 @@ typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> CO
  * @brief Tracking output [track_id, detection]
  * 
  */
-using RESULT_DATA = std::pair<int, DET>;
+using RESULT_DATA = std::pair<int, DET_VEC>;
