aa67dbaa3b3fd77314d9e21f249214cf4ae39042
Vipin Sharma <vipin.sharma2024@gmail.com>
2023-05-05 21:49:08 +0400

    replaced typedef with using keyword

--- include/DataType.h
+++ include/DataType.h
@@ -1,100 +1,90 @@
 #pragma once
 
-#include <vector>
-
+#include <cstdint>
 #include <eigen3/Eigen/Core>
 #include <eigen3/Eigen/Dense>
+#include <utility>
+#include <vector>
 
 constexpr uint8_t DET_ELEMENTS = 4;
 constexpr uint8_t FEATURE_DIM = 128;
 constexpr uint8_t KALMAN_STATE_SPACE_DIM = 8;
 constexpr uint8_t KALMAN_MEASUREMENT_SPACE_DIM = 4;
 
-
-typedef Eigen::Matrix<float, 1, DET_ELEMENTS> DET_VEC;
-typedef Eigen::Matrix<float, Eigen::Dynamic, DET_ELEMENTS> DET_MATRIX;
-
-
-// Re-ID features
+// Detection
 /**
- * @brief Re-ID feature vector
- * 
+ * @brief Detection vector with DET_ELEMENTS elements.
  */
-typedef Eigen::Matrix<float, 1, FEATURE_DIM> FEATURE_VECTOR;
+using DetVec = Eigen::Matrix<float, 1, DET_ELEMENTS>;
 /**
- * @brief Re-ID feature vector matrix [num_features, FEATURE_DIM]
- * 
+ * @brief Detection matrix with dynamic rows and DET_ELEMENTS columns.
  */
-typedef Eigen::Matrix<float, Eigen::Dynamic, FEATURE_DIM> FEATURE_MATRIX;
+using DetMatrix = Eigen::Matrix<float, Eigen::Dynamic, DET_ELEMENTS>;
 
+// Re-ID Features
+/**
+ * @brief Re-ID feature vector with FEATURE_DIM elements.
+ */
+using FeatureVector = Eigen::Matrix<float, 1, FEATURE_DIM>;
+/**
+ * @brief Re-ID feature matrix with dynamic rows and FEATURE_DIM columns.
+ */
+using FeatureMatrix = Eigen::Matrix<float, Eigen::Dynamic, FEATURE_DIM>;
 
 // Kalman Filter
 /**
- * @brief 1xKALMAX_STATE_SPACE_DIM vector
- * 
+ * @brief Kalman Filter state space vector with KALMAN_STATE_SPACE_DIM elements.
  */
-typedef Eigen::Matrix<float, 1, KALMAN_STATE_SPACE_DIM> KF_STATE_SPACE_VEC;
+using KFStateSpaceVec = Eigen::Matrix<float, 1, KALMAN_STATE_SPACE_DIM>;
 /**
- * @brief KALMAN_STATE_SPACE_DIMxKALMAN_STATE_SPACE_DIM matrix
- * 
+ * @brief Kalman Filter state space matrix with KALMAN_STATE_SPACE_DIM rows and columns.
  */
-typedef Eigen::Matrix<float, KALMAN_STATE_SPACE_DIM, KALMAN_STATE_SPACE_DIM> KF_STATE_SPACE_MATRIX;
+using KFStateSpaceMatrix = Eigen::Matrix<float, KALMAN_STATE_SPACE_DIM, KALMAN_STATE_SPACE_DIM>;
 /**
- * @brief Kalman filter state space data [mean, covariance]
- * 
+ * @brief Kalman Filter state space data containing a mean vector and a covariance matrix.
  */
-using KF_DATA_STATE_SPACE = std::pair<KF_STATE_SPACE_VEC, KF_STATE_SPACE_MATRIX>;
+using KFDataStateSpace = std::pair<KFStateSpaceVec, KFStateSpaceMatrix>;
 
 /**
- * @brief 1xKALMAN_MEASUREMENT_SPACE_DIM vector
- * 
+ * @brief Kalman Filter measurement space vector with KALMAN_MEASUREMENT_SPACE_DIM elements.
  */
-typedef Eigen::Matrix<float, 1, KALMAN_MEASUREMENT_SPACE_DIM, Eigen::RowMajor> KF_MEAS_SPACE_VEC;
+using KFMeasSpaceVec = Eigen::Matrix<float, 1, KALMAN_MEASUREMENT_SPACE_DIM, Eigen::RowMajor>;
 /**
- * @brief KALMAN_MEASUREMENT_SPACE_DIMxKALMAN_MEASUREMENT_SPACE_DIM matrix
- * 
+ * @brief Kalman Filter measurement space matrix with KALMAN_MEASUREMENT_SPACE_DIM rows and columns.
  */
-typedef Eigen::Matrix<float, KALMAN_MEASUREMENT_SPACE_DIM, KALMAN_MEASUREMENT_SPACE_DIM, Eigen::RowMajor> KF_MEAS_SPACE_MATRIX;
+using KFMeasSpaceMatrix = Eigen::Matrix<float, KALMAN_MEASUREMENT_SPACE_DIM, KALMAN_MEASUREMENT_SPACE_DIM, Eigen::RowMajor>;
 /**
- * @brief Kalman filter measurement space data [mean, covariance]
- * 
+ * @brief Kalman Filter measurement space data containing a mean vector and a covariance matrix.
  */
-using KF_DATA_MEASUREMENT_SPACE = std::pair<KF_MEAS_SPACE_VEC, KF_MEAS_SPACE_MATRIX>;
-
+using KFDataMeasurementSpace = std::pair<KFMeasSpaceVec, KFMeasSpaceMatrix>;
 
 // Tracker
 /**
- * @brief Tracker data [track_id, feature_vector]
- * 
+ * @brief Tracker data containing a track ID and a feature vector.
  */
-using TRACKER_DATA = std::pair<int, FEATURE_VECTOR>;
+using TrackerData = std::pair<int, FeatureVector>;
 /**
- * @brief Match data [track_id, detection_id]
- * 
+ * @brief Match data containing a track ID and a detection ID.
  */
-using MATCH_DATA = std::pair<int, int>;
-/**
- * @brief Association data [matches, unmatched_tracks, unmatched_detections]
- * 
- */
-typedef struct {
-    std::vector<MATCH_DATA> matches;
-    std::vector<int> unmatched_tracks;
-    std::vector<int> unmatched_detections;
-} ASSOCIATION_DATA;
+using MatchData = std::pair<int, int>;
 
+/**
+ * @brief Association data containing matched and unmatched tracks and detections.
+ */
+struct AssociationData {
+    std::vector<MatchData> matches;       ///< Matched track and detection pairs.
+    std::vector<int> unmatched_tracks;    ///< Unmatched track IDs.
+    std::vector<int> unmatched_detections;///< Unmatched detection IDs.
+};
 
 // Linear Assignment
 /**
- * @brief Cost matrix for linear assignment [num_tracks, num_detections]
- * 
+ * @brief Cost matrix for linear assignment with dynamic rows and columns.
  */
-typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> COST_MATRIX;
-
+using CostMatrix = Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
 
 // Result
 /**
- * @brief Tracking output [track_id, detection]
- * 
+ * @brief Tracking result data containing a track ID and a detection vector.
  */
-using RESULT_DATA = std::pair<int, DET_VEC>;
+using ResultData = std::pair<int, DetVec>;
