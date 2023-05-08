de8259444df48e83439268142ab5e0a687c41b6e
dumb af <vipin.sharma2024@gmail.com>
2023-05-08 10:56:38 +0400

    track construction with option feat vec support

--- include/track.h
+++ include/track.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "KalmanFilter.h"
+#include <deque>
 
 enum TrackState {
     New = 0,
@@ -12,10 +13,8 @@ enum TrackState {
 
 class Track {
 public:
-    Track(std::vector<float> tlwh, float score, uint8_t class_id);
+    Track(std::vector<float> tlwh, float score, uint8_t class_id, std::optional<FeatureVector> feat = std::nullopt, int feat_history_size = 50);
     ~Track();
-
-    void static multi_predict(std::vector<Track *> &tracks, const byte_kalman::KalmanFilter &kalman_filter);
 
     int next_id();
     int end_frame();
@@ -25,6 +24,7 @@ public:
 
     void activate(byte_kalman::KalmanFilter &kalman_filter, int frame_id);
     void re_activate(Track &new_track, int frame_id, bool new_id = false);
+    void static multi_predict(std::vector<Track *> &tracks, const byte_kalman::KalmanFilter &kalman_filter);
     void update(Track &new_track, int frame_id);
 
     bool is_activated;
@@ -44,6 +44,14 @@ private:
     std::vector<float> _tlwh;
     float _score;
     uint8_t _class_id;
+    static constexpr float _alpha = 0.9;
+
+    int _feat_history_size;
+    FeatureVector _curr_feat, _smooth_feat;
+    std::deque<FeatureVector> _feat_history;
+
 
     byte_kalman::KalmanFilter kalman_filter;
+
+    void _update_features(FeatureVector &feat);
 };
--- src/track.cpp
+++ src/track.cpp
@@ -1,6 +1,6 @@
 #include "track.h"
 
-Track::Track(std::vector<float> tlwh, float score, uint8_t class_id) {
+Track::Track(std::vector<float> tlwh, float score, uint8_t class_id, std::optional<FeatureVector> feat, int feat_history_size) {
     // Save the detection in the det_tlwh vector
     det_tlwh.resize(DET_ELEMENTS);
     det_tlwh.assign(tlwh.begin(), tlwh.end());
@@ -8,13 +8,35 @@ Track::Track(std::vector<float> tlwh, float score, uint8_t class_id) {
     _score = score;
     _class_id = class_id;
 
-    state = TrackState::New;
-    is_activated = false;
-    track_id = 0;
-
-    frame_id = 0;
     tracklet_len = 0;
-    start_frame = 0;
+    is_activated = false;
+    state = TrackState::New;
+
+    if (feat) {
+        _feat_history_size = feat_history_size;
+        _update_features(*feat);
+    }
+}
+
+Track::~Track() {
+    // Nothing to do here
+}
+
+void Track::_update_features(FeatureVector &feat) {
+    feat /= feat.norm();
+    _curr_feat = feat;
+
+    if (_feat_history.size() == 0) {
+        _smooth_feat = feat;
+    } else {
+        _smooth_feat = _alpha * _smooth_feat + (1 - _alpha) * feat;
+    }
+
+    if (_feat_history.size() == _feat_history_size) {
+        _feat_history.pop_front();
+    }
+    _feat_history.push_back(feat);
+    _smooth_feat /= _smooth_feat.norm();
 }
 
 int Track::next_id() {
