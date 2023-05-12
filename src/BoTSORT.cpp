dd5270344d9798838196bd27e46c1e0d9a63a026
dumb af <vipin.sharma2024@gmail.com>
2023-05-12 12:33:07 +0400

    added code for 2nd level of assocition

--- src/BoTSORT.cpp
+++ src/BoTSORT.cpp
@@ -44,7 +44,7 @@ BoTSORT::BoTSORT(
 }
 
 std::vector<Track> BoTSORT::track(const std::vector<Detection> &detections, const cv::Mat &frame) {
-    ////////////////// Step 1: Create tracks for detections //////////////////
+    ////////////////// CREATE TRACK OBJECT FOR ALL THE DETECTIONS //////////////////
     // For all detections, extract features, create tracks and classify on the segregate of confidence
     _frame_id++;
     std::vector<Track *> detections_high_conf, detections_low_conf;
@@ -83,9 +83,10 @@ std::vector<Track> BoTSORT::track(const std::vector<Detection> &detections, cons
             tracked_tracks.push_back(track);
         }
     }
+    ////////////////// CREATE TRACK OBJECT FOR ALL THE DETECTIONS //////////////////
 
 
-    ////////////////// Step 2: First association, with high score detection boxes //////////////////
+    ////////////////// Apply KF predict and GMC before running assocition algorithm //////////////////
     // Merge currently tracked tracks and lost tracks
     std::vector<Track *> tracks_pool;
     tracks_pool = _merge_track_lists(tracked_tracks, _lost_tracks);
@@ -97,37 +98,101 @@ std::vector<Track> BoTSORT::track(const std::vector<Detection> &detections, cons
     HomographyMatrix H = _gmc_algo->apply(frame, detections);
     Track::multi_gmc(tracks_pool, H);
     Track::multi_gmc(unconfirmed_tracks, H);
+    ////////////////// Apply KF predict and GMC before running assocition algorithm //////////////////
 
-    // Associate tracks with high confidence detections
+
+    ////////////////// ASSOCIATION ALGORITHM STARTS HERE //////////////////
+
+    ////////////////// First association, with high score detection boxes //////////////////
     CostMatrix iou_dists, raw_emd_dist;
 
+    // Find IoU distance between all tracked tracks and high confidence detections
     iou_dists = iou_distance(tracks_pool, detections_high_conf);
-    fuse_score(iou_dists, detections_high_conf);
+    fuse_score(iou_dists, detections_high_conf);// Fuse the score with IoU distance
 
     if (_reid_enabled) {
+        // If re-ID is enabled, find the embedding distance between all tracked tracks and high confidence detections
         raw_emd_dist = embedding_distance(tracks_pool, detections_high_conf);
-        fuse_motion(*_kalman_filter, raw_emd_dist, tracks_pool, detections_high_conf, false, _lambda);
+        fuse_motion(*_kalman_filter, raw_emd_dist, tracks_pool, detections_high_conf, false, _lambda);// Fuse the motion with embedding distance
     }
 
-    AssociationData first_associations;
+    // Fuse the IoU distance and embedding distance to get the final distance matrix
     CostMatrix distances_first_association = fuse_iou_with_emb(iou_dists, raw_emd_dist, _proximity_thresh, _appearance_thresh);
+
+    // Perform linear assignment on the final distance matrix, LAPJV algorithm is used here
+    AssociationData first_associations;
     linear_assignment(distances_first_association, _match_thresh, first_associations);
 
+    // Update the tracks with the associated detections
     for (size_t i = 0; i < first_associations.matches.size(); i++) {
         Track *track = tracks_pool[first_associations.matches[i].first];
         Track *detection = detections_high_conf[first_associations.matches[i].second];
 
+        // If track was being actively tracked, we update the track with the new associated detection
         if (track->state == TrackState::Tracked) {
             track->update(*_kalman_filter, *detection, _frame_id);
             activated_tracks.push_back(track);
         } else {
+            // If track was not being actively tracked, we re-activate the track with the new associated detection
+            // NOTE: There should be a minimum number of frames before a track is re-activated
+            track->re_activate(*_kalman_filter, *detection, _frame_id, false);
+            refind_tracks.push_back(track);
+        }
+    }
+    ////////////////// First association, with high score detection boxes //////////////////
+
+
+    ////////////////// Second association, with low score detection boxes //////////////////
+    // Get all unmatched but tracked tracks after the first association, these tracks will be used for the second association
+    std::vector<Track *> unmatched_tracks_after_1st_association;
+    for (size_t i = 0; i < first_associations.unmatched_track_indices.size(); i++) {
+        int track_idx = first_associations.unmatched_track_indices[i];
+        Track *track = tracks_pool[track_idx];
+        if (track->state == TrackState::Tracked) {
+            unmatched_tracks_after_1st_association.push_back(track);
+        }
+    }
+
+    // Find IoU distance between unmatched but tracked tracks left after the first association and low confidence detections
+    CostMatrix iou_dists_second;
+    iou_distance(unmatched_tracks_after_1st_association, detections_low_conf);
+
+    // Perform linear assignment on the final distance matrix, LAPJV algorithm is used here
+    AssociationData second_associations;
+    linear_assignment(iou_dists_second, 0.5, second_associations);
+
+    // Update the tracks with the associated detections
+    for (size_t i = 0; i < second_associations.matches.size(); i++) {
+        Track *track = unmatched_tracks_after_1st_association[second_associations.matches[i].first];
+        Track *detection = detections_low_conf[second_associations.matches[i].second];
+
+        // If track was being actively tracked, we update the track with the new associated detection
+        if (track->state == TrackState::Tracked) {
+            track->update(*_kalman_filter, *detection, _frame_id);
+            activated_tracks.push_back(track);
+        } else {
+            // If track was not being actively tracked, we re-activate the track with the new associated detection
+            // NOTE: There should be a minimum number of frames before a track is re-activated
             track->re_activate(*_kalman_filter, *detection, _frame_id, false);
             refind_tracks.push_back(track);
         }
     }
 
+    // The tracks that are not associated with any detection even after the second association are marked as lost
+    std::vector<Track *> lost_tracks;
+    for (size_t i = 0; i < second_associations.unmatched_track_indices.size(); i++) {
+        Track *track = unmatched_tracks_after_1st_association[second_associations.unmatched_track_indices[i]];
+        if (track->state != TrackState::Lost) {
+            track->mark_lost();
+            lost_tracks.push_back(track);
+        }
+    }
+    ////////////////// Second association, with low score detection boxes //////////////////
 
-    ////////////////// Step 3: Second association, with low score detection boxes //////////////////
+
+    ////////////////// Deal with unconfirmed tracks //////////////////
+    ////////////////// Deal with unconfirmed tracks //////////////////
+
 
     // Added for code compilation
     return std::vector<Track>();
