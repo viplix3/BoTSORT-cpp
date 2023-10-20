982d3d56c259e96a8676316d76492f4f6c53633d
Vipin Sharma <vipin.sharma2024@gmail.com>
2023-10-20 23:08:14 +0400

    maybe this would work

--- botsort/include/TensorRT_InferenceEngine.h
+++ botsort/include/TensorRT_InferenceEngine.h
@@ -3,5 +3,43 @@
 #include <memory>
 #include <string>
 
-#include "NvInfer.h"
 #include <opencv2/opencv.hpp>
+
+#include "NvInfer.h"
+
+static auto StreamDeleter = [](cudaStream_t *ptr) {
+    if (ptr)
+    {
+        cudaStreamDestroy(*ptr);
+        delete ptr;
+    }
+};
+
+struct TRTDeleter
+{
+    template<typename T>
+    void operator()(T *obj) const
+    {
+        if (obj) { obj->destroy(); }
+    }
+};
+
+
+struct TRTDestroyer
+{
+    template<typename T>
+    void operator()(T *obj) const
+    {
+        if (obj) { obj->destroy(); }
+    }
+};
+
+template<typename T>
+using TRTUniquePtr = std::unique_ptr<T, TRTDeleter>;
+
+template<typename T>
+inline TRTUniquePtr<T> infer_object(T *obj)
+{
+    if (!obj) { throw std::runtime_error("Failed to create object"); }
+    return TRTUniquePtr<T>(obj);
+}
